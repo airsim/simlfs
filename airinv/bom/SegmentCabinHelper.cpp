cb0d5743c5e381e0569b622e4effa6b1d0d1bc4e
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-10 08:58:44 +0100

    [AirInv] Integrated latest work on sale support.

--- airinv/bom/SegmentCabinHelper.cpp
+++ airinv/bom/SegmentCabinHelper.cpp
@@ -8,11 +8,44 @@
 #include <stdair/bom/FlightDate.hpp>
 #include <stdair/bom/SegmentCabin.hpp>
 #include <stdair/bom/LegCabin.hpp>
+#include <stdair/bom/BookingClass.hpp>
 // AIRINV
 #include <airinv/bom/SegmentCabinHelper.hpp>
 #include <airinv/bom/FlightDateHelper.hpp>
 
 namespace AIRINV {
+  // ////////////////////////////////////////////////////////////////////
+  void SegmentCabinHelper::
+  initialiseAU (stdair::SegmentCabin& iSegmentCabin) {
+    // Initialise the capacity and availability pool.
+    const stdair::LegCabinList_T& lLCList =
+      stdair::BomManager::getList<stdair::LegCabin> (iSegmentCabin);
+    stdair::CabinCapacity_T lCapacity =
+      std::numeric_limits<stdair::CabinCapacity_T>::max();
+    for (stdair::LegCabinList_T::const_iterator itLC = lLCList.begin();
+         itLC != lLCList.end(); ++itLC) {
+      const stdair::LegCabin* lLC_ptr = *itLC;
+      assert (lLC_ptr != NULL);
+      const stdair::CabinCapacity_T& lCabinCap = lLC_ptr->getOfferedCapacity ();
+      if (lCapacity > lCabinCap) {
+        lCapacity = lCabinCap;
+      }
+    }
+    iSegmentCabin.setCapacity (lCapacity);
+    iSegmentCabin.setAvailabilityPool (lCapacity);
+    
+    // Browse the list of booking classes and set the AU of each booking
+    // class to the availability pool of the cabin.
+    const stdair::BookingClassList_T& lBCList =
+      stdair::BomManager::getList<stdair::BookingClass> (iSegmentCabin);
+    for (stdair::BookingClassList_T::const_iterator itBC = lBCList.begin();
+         itBC != lBCList.end(); ++itBC) {
+      stdair::BookingClass* lBC_ptr = *itBC;
+      assert (lBC_ptr != NULL);
+      lBC_ptr->setAuthorizationLevel (lCapacity);
+    }
+  }
+  
   // ////////////////////////////////////////////////////////////////////
   void SegmentCabinHelper::
   updateFromReservation (const stdair::FlightDate& iFlightDate,
